ec8dfd159a4300f6743a7eb79bece2a8e0ddab6c
wkwkrnht <wkwkrnht@gmail.com>
2019-04-27 01:23:33 +0900

    optimize

--- config.rb
+++ config.rb
@@ -1,18 +1,15 @@
-#Bootstrap is used to style bits of the demo. Remove it from the config, gemfile and stylesheets to stop using bootstrap
-require "uglifier"
-
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
 # Use '#id' and '.classname' as div shortcuts in slim
 # http://slim-lang.com/
 Slim::Engine.set_options shortcut: {
-  '#' => {tag: 'div', attr: 'id'},
-  '.' => {tag: 'div', attr: 'class'}
+    '#' => {tag: 'div', attr: 'id'},
+    '.' => {tag: 'div', attr: 'class'}
 }
 
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+    prefix.browsers = "last 2 versions"
 end
 
 activate :livereload
@@ -27,9 +24,11 @@ page "/partials/*", layout: false
 page "/admin/*", layout: false
 
 activate :blog do |blog|
-  blog.permalink = "news/{year}/{title}.html"
-  blog.sources = "posts/{title}.html"
-  blog.layout = "news-detail"
+    blog.permalink = "news/{year}/{title}.html"
+    blog.sources = "posts/{title}.html"
+    blog.layout = "article"
+    blog.tag_template = 'layouts/tag'
+    blog.calendar_template = 'layouts/calender'
 end
 
 # With alternative layout
@@ -38,13 +37,13 @@ end
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
-# proxy product.yml files to product.html 
+# proxy product.yml files to product.html
 data.products.each do |product|
   # product is an array: [filename, {data}]
-  proxy "/product/#{product[1][:title].parameterize}/index.html", "product.html", 
-  locals: {product: product[1]}, 
-  layout: 'product-detail',
-  ignore: true
+    proxy "/product/#{product[1][:title].parameterize}/index.html", "product.html",
+    locals: {product: product[1]},
+    layout: 'product-detail',
+    ignore: true
 end
 
 # Helpers
@@ -56,35 +55,33 @@ activate :directory_indexes
 
 helpers do
   #helper to set background images with asset hashes in a style attribute
-  def background_image(image)
-    "background-image: url('" << image_path(image) << "')"
-  end
-  
-  def nav_link(link_text, url, options = {})
-    options[:class] ||= ""
-    options[:class] << " active" if url == current_page.url
-    link_to(link_text, url, options)
-  end
+    def background_image(image)
+        "background-image: url('" << image_path(image) << "')"
+    end
 
-  def markdown(content)
-     Tilt['markdown'].new { content }.render
-  end
+    def nav_link(link_text, url, options = {})
+        options[:class] ||= ""
+        options[:class] << " active" if url == current_page.url
+        link_to(link_text, url, options)
+    end
+
+    def markdown(content)
+        Tilt['markdown'].new { content }.render
+    end
 end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
 configure :build do
-  # Minify css on build
-  activate :minify_css
+    # Minify HTML, css, Javascript on build
+    activate :minify_html
+    activate :minify_css
+    activate :minify_javascript, :ignore => "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
 
-  # Minify Javascript on build
-  activate :minify_javascript, :ignore => "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
+    # Use Gzip
+    activate :gzip
 
-  # Use Gzip
-  activate :gzip
-
-  #Use asset hashes to use for caching
-  #activate :asset_hash
-
+    #Use asset hashes to use for caching
+    activate :asset_hash
 end
