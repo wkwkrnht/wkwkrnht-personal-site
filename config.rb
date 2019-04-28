921e555cba964e32b1823d8e022a9a0e554970ba
wkwkrnht <wkwkrnht@gmail.com>
2019-04-28 22:47:34 +0900

    fix

--- config.rb
+++ config.rb
@@ -1,14 +1,27 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 require "uglifier"
 
-# Use '#id' and '.classname' as div shortcuts in slim
-# http://slim-lang.com/
-Slim::Engine.set_options shortcut: {
-    '#' => {tag: 'div', attr: 'id'},
-    '.' => {tag: 'div', attr: 'class'}
-}
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+# Per-page layout changes
+# page '/path/to/file.html', layout: 'other_layout'
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page "/partials/*", layout: false
+page "/admin/*", layout: false
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+# proxy product.yml files to product.html
+#data.products.each do |product|
+    # product is an array: [filename, {data}]
+#    proxy "/product/#{product[1][:title].parameterize}/index.html", "product.html",
+#    locals: {product: product[1]},
+#    layout: 'product-detail',
+#    ignore: true
+#end
 
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 activate :autoprefixer do |prefix|
     prefix.browsers = "last 2 versions"
 end
@@ -16,7 +29,7 @@ activate :automatic_image_sizes
 activate :syntax, :line_numbers => true
 activate :directory_indexes # pretty urls
 activate :sitemap_ping do |config|
-    config.host = 'https://wkwkrnht-blog-by-middleman.netlify.com' # (required) Host of your website
+    config.host = 'https://wkwkrnht-blog-test.netlify.com/' # (required) Host of your website
 end
 activate :robots,
     :rules => [
@@ -31,44 +44,20 @@ activate :robots,
             :disallow => %w(404.html)
         }
     ],
-    :sitemap => 'https://wkwkrnht-blog-by-middleman.netlify.com/sitemap.xml'
-
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
-# page '/path/to/file.html', layout: 'other_layout'
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-page "/partials/*", layout: false
-page "/admin/*", layout: false
-
+    :sitemap => 'https://wkwkrnht-blog-test.netlify.com/sitemap.xml'
 activate :blog do |blog|
     blog.default_extension = '.md'
-    blog.permalink = "blog/{year}/{title}.html"
-    blog.sources = "blog/articles/{title}.html"
+    blog.permalink = 'blog/:year/:title.html'
+    blog.sources = 'blog/articles/:title.html'
     blog.layout = 'article'
     blog.tag_template = 'layouts/tag'
     blog.calendar_template = 'layouts/calender'
 end
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-# proxy product.yml files to product.html
-#data.products.each do |product|
-    # product is an array: [filename, {data}]
-#    proxy "/product/#{product[1][:title].parameterize}/index.html", "product.html",
-#    locals: {product: product[1]},
-#    layout: 'product-detail',
-#    ignore: true
-#end
-
 # Helpers
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 helpers do
-  #helper to set background images with asset hashes in a style attribute
     def background_image(image)
         "background-image: url('" << image_path(image) << "')"
     end
@@ -81,7 +70,6 @@ end
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 configure :build do
-    # Minify HTML, css, Javascript on build
     activate :minify_html
     activate :minify_css
     activate :minify_javascript, :ignore => "**/admin/**", compressor: ::Uglifier.new(mangle: true, compress: { drop_console: true }, output: {comments: :none})
