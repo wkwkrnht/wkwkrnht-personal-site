eee58cdaf236df64e92cb5cc33d694824079df74
wkwkrnht <wkwkrnht@gmail.com>
2019-05-02 16:57:02 +0900

    test

--- config.rb
+++ config.rb
@@ -25,12 +25,12 @@ page "/admin/*", layout: false
 #activate :embed
 activate :automatic_image_sizes
 activate :syntax, :line_numbers => true
-activate :blog do |blog|
-    blog.sources = "blog/articles/*.html"
-    blog.default_extension = '.md'
-    blog.layout = 'layouts/article'
-    blog.permalink = "{year}/{month}/{day}.html"
-end
+#activate :blog do |blog|
+#    blog.sources = "blog/articles/*.html"
+#    blog.default_extension = '.md'
+#    blog.layout = 'layouts/article'
+#    blog.permalink = "{year}/{month}/{day}.html"
+#end
 
 # Build-specific configuration: https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 configure :build do
