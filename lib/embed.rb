0175f117e611bafd16f9eb5d2d042d94a742b4c7
wkwkrnht <wkwkrnht@gmail.com>
2019-05-10 23:29:02 +0900

    fix

--- lib/embed.rb
+++ lib/embed.rb
@@ -5,27 +5,27 @@ class String
     def embed_youtube!()
         #(HOW CALL:youtube {videoID},E.X.:youtube aLvJ1mqlM98) -> <figure class="embed-responsive embed-responsive-16by9"><iframe src="//www.youtube.com/embed/aLvJ1mqlM98" frameborder="0" allowfullscreen></iframe></figure>
         embed43 = "<figure class='embed-responsive embed-4x3'><iframe src='//www.youtube.com/embed/\\1' frameborder='0' allowfullscreen></iframe></figure>"
-        replace self.gsub(/YOUTUBE43 [^#\&\?<]+/,embed43)
+        replace self.gsub(/YOUTUBE43 ([^#\&\?<]+)/,embed43)
         embed169 = "<figure class='embed-responsive embed-16x9'><iframe src='//www.youtube.com/embed/\\1' frameborder='0' allowfullscreen></iframe></figure>"
-        replace self.gsub(/YOUTUBE169 [^#\&\?<]+/,embed169)
+        replace self.gsub(/YOUTUBE169 ([^#\&\?<]+)/,embed169)
     end
 
     def embed_spotify!()
         #(HOW CALL:spotify {URL},E.X.:spotify https://google.com) -> <figure class="embed-responsive spotifycard"><iframe src="https://embed.spotify.com/?uri=" width="300" height="380" frameborder="0" allowtransparency="true"></iframe></figure>
         embed = "<figure class='embed-responsive spotifycard'><iframe src='https://embed.spotify.com/?uri=\\1' width='300' height='380' frameborder='0' allowtransparency='true'></iframe></figure>"
-        replace self.gsub(/SPOTIFY [^#\&\?<]+/,embed)
+        replace self.gsub(/SPOTIFY ([^#\&\?<]+)/,embed)
     end
 
     def embed_hatena!()
         #(HOW CALL:hatena {URL},E.X.:hatena https://google.com) -> <figure class="embed-responsive hatenablogcard"><iframe src="https://hatenablog-parts.com/embed?url=https://google.com" frameborder="0" scrolling="no"></iframe></figure>
         embed = "<figure class='embed-responsive hatenablogcard'><iframe src='https://hatenablog-parts.com/embed?url=\\1' frameborder='0' scrolling='no'></iframe></figure>"
-        replace self.gsub(/HATENA [^#\&\?<]+/,embed)
+        replace self.gsub(/HATENA ([^#\&\?<]+)/,embed)
     end
 
     def embed_embedly!()
         #(HOW CALL:embedly {URL},E.X.:embedly https://google.com) -> <a class='embedly-card' href='https://google.com'>https://google.com</a><script async='' src='//cdn.embedly.com/widgets/platform.js'></script>"
         embed = "<a class='embedly-card' href='\\1'></a><script async='' src='//cdn.embedly.com/widgets/platform.js'></script>"
-        replace self.gsub(/EMBEDLY [^#\&\?<]+/,embed)
+        replace self.gsub(/EMBEDLY ([^#\&\?<]+)/,embed)
     end
 
     def embed_items!
