2852d8c17a8171fb574a0b7b74330f4c57640bc6
wkwkrnht <wkwkrnht@gmail.com>
2019-05-13 00:44:35 +0900

    add todo

--- lib/short-code.rb
+++ lib/short-code.rb
@@ -6,9 +6,7 @@ module Middleman
         def initialize(app,options_hash={},&block)
             super
             app.after_render do |body, path, locs, template|
-                if (path.to_s.index "blog_post") != nil
-                    body.shortcode_extract!
-                end
+                body.shortcode_extract!
                 body
             end
         end
@@ -18,11 +16,27 @@ end
 
 class String
     def shortcode_extract!
-        #extract_code_embed!('code')
+        #extract_code_embed!()
     end
 
-    #def extract_code_embed!(match)
+    #def extract_columun_embed!(match)
     #    shortcode = "<pre class='prettyprint linenums'><code>\\1</code></pre><script async='' src='//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=doxy'></script>"
-    #    replace self.gsub(/<code>#{match}<\/code>/,shortcode)
+    #    replace self.gsub(/columun ([^#\&\?<]+) ([^#\&\?<]+)/,shortcode)
+    #end
+    #def extract_notice_embed!(match)
+    #    shortcode = "<pre class='prettyprint linenums'><code>\\1</code></pre><script async='' src='//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=doxy'></script>"
+    #    replace self.gsub(/notice ([^#\&\?<]+)/,shortcode)
+    #end
+    #def extract_question_embed!(match)
+    #    shortcode = "<pre class='prettyprint linenums'><code>\\1</code></pre><script async='' src='//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=doxy'></script>"
+    #    replace self.gsub(/question ([^#\&\?<]+)/,shortcode)
+    #end
+    #def extract_search_embed!(match)
+    #    shortcode = "<pre class='prettyprint linenums'><code>\\1</code></pre><script async='' src='//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=doxy'></script>"
+    #    replace self.gsub(/search ([^#\&\?<]+)/,shortcode)
+    #end
+    #def extract_search_embed!(match)
+    #    shortcode = "<pre class='prettyprint linenums'><code>\\1</code></pre><script async='' src='//cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=doxy'></script>"
+    #    replace self.gsub(/button ([^#\&\?<]+) ([^#\&\?<]+)/,shortcode)
     #end
 end
