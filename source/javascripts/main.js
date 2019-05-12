1aa5acabd2c53eb93872e8c017626153f58a224e
wkwkrnht <wkwkrnht@gmail.com>
2019-05-13 00:03:52 +0900

    test

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,6 +1,6 @@
-(document.getElementById('nativeShare').onclick = function(){
+document.getElementById('nativeShare').onclick = function(){
     let title = document.title;
     let text = document.getElementsByName('description').item(0).content;
     let url = window.location.href;
     navigator.share({title: title, text: text, url: url}).catch(function(error){console.error('Error sharing: ' + error)});
-};)();
+};
