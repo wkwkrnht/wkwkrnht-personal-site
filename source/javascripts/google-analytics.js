94a56c2bb95596fda82ddbc566b64f4ed9195558
wkwkrnht <wkwkrnht@gmail.com>
2019-05-19 18:07:59 +0900

    fix

--- source/javascripts/google-analytics.js
+++ source/javascripts/google-analytics.js
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
         var accept = document.createElement('div');
         accept.setAttribute('class', 'module-ga-cookie-accept-bar');
         accept.setAttribute('id', 'name-ga-cookie-accept-bar');
-        accept.innerHTML = '<p><i class="material-icons" aria-hidden="true">warning</i>このサイトでは Google アナリティクスの Cookie（クッキー）を使用して、ユーザーのWebサイト閲覧データを記録しています。<a href="./"><i class="material-icons" aria-hidden="true">chevron_right</i>プライバシーポリシーを確認</a></p><p><button id="name-ga-cookie-accept-btn" class="module-ga-cookie-accept-btn"><i class="material-icons" aria-hidden="true">check</i>同意して Cookie を受け入れる</button> <button id="name-ga-cookie-deny-btn" class="module-ga-cookie-accept-btn module-ga-cookie-deny-btn"><i class="material-icons" aria-hidden="true">close</i>同意しない</button></p>';
+        accept.innerHTML = '<p><i class="material-icons" aria-hidden="true"></i>このサイトでは Google アナリティクスの Cookie（クッキー）を使用して、ユーザーのWebサイト閲覧データを記録しています。<a href="/privacy-policy"><i class="material-icons" aria-hidden="true">chevron_right</i>プライバシーポリシーを確認</a></p><p><button id="name-ga-cookie-accept-btn" class="module-ga-cookie-accept-btn"><i class="material-icons" aria-hidden="true">check</i>同意して Cookie を受け入れる</button> <button id="name-ga-cookie-deny-btn" class="module-ga-cookie-accept-btn module-ga-cookie-deny-btn"><i class="material-icons" aria-hidden="true">close</i>同意しない</button></p>';
         document.body.appendChild(accept);
     }
 
