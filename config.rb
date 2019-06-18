43aa57c8b7d93377683f4db2f51db1b1bbb6d3bd
wkwkrnht <wkwkrnht@gmail.com>
2019-06-18 18:43:52 +0900

    fix

--- config.rb
+++ config.rb
@@ -2,27 +2,27 @@ require 'lib/short-code.rb'
 
 site_data = data.front
 
-def make_rss(site_data)
-    require 'rss'
-    rss = RSS::Maker.make('atom') do |maker|
-        maker.channel.about = site_data.baseurl + '/sitemap.xml'
-        maker.channel.title = site_data.title
-        maker.channel.description = site_data.description
-        maker.channel.link = site_data.base_url
-        maker.channel.author = site_data.author || site_data.title
-        maker.channel.update = Time.now
-        maker.items.do_sort = true
-        page_articles.each_with_index do |article, i|
-            maker.items.new_item do |item|
-                item.link = article.url
-                item.title = article.title
-                item.date = Time.parse(article.date)
-                item.description = article.description
-            end
-        end
-    end
-    File.open(Rails.root.join('build', 'sitemap.xml'), 'w') { |f| f.puts(rss) }
-end
+#def make_rss(site_data)
+#    require 'rss'
+#    rss = RSS::Maker.make('atom') do |maker|
+#        maker.channel.about = site_data.baseurl + '/sitemap.xml'
+#        maker.channel.title = site_data.title
+#        maker.channel.description = site_data.description
+#        maker.channel.link = site_data.base_url
+#        maker.channel.author = site_data.author || site_data.title
+#        maker.channel.update = Time.now
+#        maker.items.do_sort = true
+#        page_articles.each_with_index do |article, i|
+#            maker.items.new_item do |item|
+#                item.link = article.url
+#                item.title = article.title
+#                item.date = Time.parse(article.date)
+#                item.description = article.description
+#            end
+#        end
+#    end
+#    File.open(Rails.root.join('build', 'sitemap.xml'), 'w') { |f| f.puts(rss) }
+#end
 
 set :markdown, :fenced_code_blocks => true, :autolink => true, :with_toc_data => true
 
