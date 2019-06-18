23fe42d38e5d66dfcd29c1cc7250ff3d620eab7f
wkwkrnht <wkwkrnht@gmail.com>
2019-06-18 18:18:51 +0900

    fix

--- config.rb
+++ config.rb
@@ -1,7 +1,25 @@
 require 'lib/short-code.rb'
 
-def make_rss()
-    site_data = data.front
+site_data = data.front
+
+def make_rss(site_data)
+    require "rss"
+    rss = RSS::Maker.make('atom') do |maker|
+        maker.channel.about = site_data.baseurl + '/about'
+        maker.channel.title = site_data.title
+        maker.channel.description = site_data.description
+        maker.channel.link = site_data.base_url
+        maker.channel.author = site_data.author || site_data.title
+        maker.channel.date = Time.now
+
+        page_articles.each_with_index do |article, i|
+            maker.items.new_item do |item|
+                item.link = article.url
+                item.title = article.title
+                item.date = Time.parse(article.date)
+            end
+        end
+    end
 end
 
 set :markdown, :fenced_code_blocks => true, :autolink => true, :with_toc_data => true
@@ -26,7 +44,7 @@ end
 # Build-specific configuration: https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 configure :build do
     activate :sitemap_ping do |config|
-        config.host = "https://wkwkrnht-blog-test.netlify.com/" # (required) Host of your website
+        config.host = site_data.base_url
     end
     activate :minify_html
     activate :gzip
