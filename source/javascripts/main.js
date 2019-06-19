a72a67495014a8b6e801ba9af77661bc53ada002
wkwkrnht <wkwkrnht@gmail.com>
2019-06-19 23:45:12 +0900

    fix

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -10,6 +10,6 @@ if(shareBtn){
 
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/javascripts/service-worker.js')
-    .then(function() { console.log("Service Worker Registered"); })
-    .catch(function() { console.log("Service Worker Not Registered"); });
+    .then(function() { console.log('Service Worker Registered'); })
+    .catch(function() { console.log('Service Worker Not Registered'); });
 }
