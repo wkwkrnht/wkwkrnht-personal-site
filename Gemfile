adbce29c627808e2752af35568620167ad93133d
wkwkrnht <wkwkrnht@gmail.com>
2019-12-17 20:58:20 +0900

    test

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'middleman'
 gem 'middleman-blog'
-gem 'middleman-img_loading_attribute'
 gem 'middleman-sitemap-ping'
 gem 'middleman-minify-html'
 gem 'slim'
--- lib/short-code.rb
+++ lib/short-code.rb
@@ -1,5 +1,44 @@
 require 'cgi'
 require 'middleman-core'
+require 'nokogiri'
+
+class Middleman::ImgLoadingAttribute < ::Middleman::Extension
+  option :loading, 'auto', 'A value of "loading" attribute in <img> tag'
+
+    def initialize(app, options_hash={}, &block)
+        # Call super to build options from the options_hash
+        super
+        # Require libraries only when activated
+        # require 'necessary/library'
+        # set up your extension
+        # puts options.my_option
+        options[:loading] = 'auto'
+    end
+    def after_configuration
+        #do something
+    end
+
+    def after_build(builder)
+        files = Dir.glob(File.join(app.config[:build_dir], "**", "*.html"))
+        files.each do |file|
+            doc = Nokogiri::HTML(File.read(file))
+            doc.css('img').each do |elem|
+                next if elem.path.include?('pre') || elem.path.include?('code') || elem.path.include?('blockquote')
+                elem['loading'] = options[:loading]
+            end
+            File.open(file, 'w') do |f|
+                f.write doc.to_html
+            end
+        end
+    end
+    # A Sitemap Manipulator
+    # def manipulate_resource_list(resources)
+    # end
+    # helpers do
+    #   def a_helper
+    #   end
+    # end
+end
 
 class String
     def extract_columun_embed!()
