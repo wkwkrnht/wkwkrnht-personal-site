ac90115c38ac00f8e9ecfbcf0a85bc5ddbc79666
wkwkrnht <wkwkrnht@gmail.com>
2020-10-12 03:03:09 +0900

    test

--- Gemfile
+++ Gemfile
@@ -2,7 +2,4 @@ source 'https://rubygems.org'
 
 gem 'middleman'
 gem 'middleman-blog'
-gem 'middleman-sitemap-ping'
-gem 'middleman-minify-html'
 gem 'slim'
-gem 'nokogiri'
--- config.rb
+++ config.rb
@@ -1,6 +1,5 @@
 require "lib/short-code.rb"
 
-set :encoding, "utf-8"
 set :markdown, :fenced_code_blocks => true, :autolink => true, :with_toc_data => true
 
 page "/*.xml", layout: false
@@ -15,7 +14,7 @@ activate :imgattribute do |img|
     img.loading = 'lazy'
 end
 activate :blog do |blog|
-    blog.sources = "blog/articles/{date}.html"
+    blog.sources = "blog/articles/{title}.html"
     blog.default_extension = ".md"
     blog.layout = "article"
     blog.tag_template = "partials/tag"
@@ -23,9 +22,5 @@ activate :blog do |blog|
 end
 
 configure :build do
-    #activate :sitemap_ping do |config|
-    #    config.host = "https://wkwkrnht.netlify.com/"
-    #end
-    #activate :minify_html
     activate :gzip
 end
